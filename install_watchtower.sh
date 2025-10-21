b283707895e526a3e613796b3a209bb70b6730b6
Optrex-ActiMist <106432219+Optrex-ActiMist@users.noreply.github.com>
2025-10-22 03:20:43 +0900

    Enable cleanup option for Watchtower

--- install_watchtower.sh
+++ install_watchtower.sh
@@ -1,4 +1,5 @@
 docker run -d --name watchtower --restart unless-stopped \
     -v /var/run/docker.sock:/var/run/docker.sock \
+    -e WATCHTOWER_CLEANUP=true \
     -e WATCHTOWER_SCHEDULE="0 0 */2 * * *" \
     containrrr/watchtower
